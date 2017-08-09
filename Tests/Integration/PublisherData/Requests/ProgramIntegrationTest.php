a9ef7e34f15a4535fa0cb6ed2038f318905781be
Thomas Helmrich <thomas@gigabit.de>
2017-08-09 22:02:31 +0200

    added program rates endpoint

--- Tests/Integration/PublisherData/Requests/ProgramIntegrationTest.php
+++ Tests/Integration/PublisherData/Requests/ProgramIntegrationTest.php
@@ -40,12 +40,19 @@ class ProgramIntegrationTest extends \PHPUnit_Framework_TestCase {
         $this->assertNotNull($programs->getPrograms());
     }
 
-    public function testGetPrograCategoriesSuccess() {
+    public function testGetProgramCategoriesSuccess() {
         $this->setUp();
         $search = new \Affilinet\PublisherData\Requests\ProgramsRequest($this->affilinetClient);
         $programCats = $search->getProgramCategories();
-
         $this->assertInstanceOf(\Affilinet\PublisherData\Responses\ProgramCategoriesResponse::class, $programCats);
         $this->assertNotNull($programCats);
+    }
+    public function testGetProgramRatesSuccess() {
+        $this->setUp();
+        $search = new \Affilinet\PublisherData\Requests\ProgramsRequest($this->affilinetClient);
+        $programRates = $search->getProgramRates(6843);
+
+        $this->assertInstanceOf(\Affilinet\PublisherData\Responses\ProgramRatesResponse::class, $programRates);
+        $this->assertNotNull($programRates);
     }
 }
--- src/Affilinet/PublisherData/Requests/ProgramsRequest.php
+++ src/Affilinet/PublisherData/Requests/ProgramsRequest.php
@@ -6,6 +6,7 @@ use Affilinet\PublisherData\AffilinetPublisherClient;
 use Affilinet\PublisherData\Requests\Traits\PaginationTrait;
 use Affilinet\PublisherData\Requests\Traits\ProgramsQueryTrait;
 use Affilinet\PublisherData\Responses\ProgramCategoriesResponse;
+use Affilinet\PublisherData\Responses\ProgramRatesResponse;
 use Affilinet\PublisherData\Responses\ProgramsResponse;
 use Affilinet\Requests\AbstractSoapRequest;
 
@@ -69,6 +70,27 @@ class ProgramsRequest extends AbstractSoapRequest {
         $programs = $this->send("GetProgramCategories");
 
         return new ProgramCategoriesResponse($programs);
+    }
+
+
+    /**
+     * @param int $programId The Id of the Programm to get the rates for
+     * @param int|null $publisherId The Publishers ID
+     *
+     * @return ProgramRatesResponse
+     */
+    public function getProgramRates($programId, $publisherId = null) {
+
+        if ($publisherId == null) {
+            $publisherId = $this->affilinetClient->getPublisherId();
+        }
+
+        $programs = $this->send("GetProgramRates", array(
+            'PublisherId' => $publisherId,
+            'ProgramId' => $programId,
+        ));
+
+        return new ProgramRatesResponse($programs);
     }
 
 }
